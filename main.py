be962c389688fce614260e79e52f7ecaa292aabe
claudcelle <claudio.cellerini@gmail.com>
2025-10-08 09:35:51 +0200

    E: new files: .gitignore,circleshape, player, modified:constants, main (draw player)

--- main.py
+++ main.py
@@ -3,19 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # print("Starting Asteroids!" )
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
+    pygame.init() 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
+    player_1 = Player(x = SCREEN_WIDTH/2,
+                      y = SCREEN_HEIGHT/2)
     while True: 
+        """ Allows to close the window with close button"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+
+        player_1.draw(screen=screen)
+
         pygame.display.flip()
+        dt = clock.tick(60)/1000 #*.tick() pause game loop until 1/60th of a second has passed. Returns time passed since last call (dt). Divide by 1000 (milliseconds -> seconds)
 
 
 if __name__ == "__main__":
